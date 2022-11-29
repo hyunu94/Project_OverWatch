7bdf6fd7cff3fd199a8c5d7ece5afc2fc63e5314
EZEN202 <EZEN202@DESKTOP-OI1ALT1>
2022-11-29 15:20:35 +0900

    bbs sql 매핑중~~

--- Project_OverWatch/src/main/java/pack/spring/project/board/BoardDAO.java
+++ Project_OverWatch/src/main/java/pack/spring/project/board/BoardDAO.java
@@ -1,5 +1,28 @@
 package pack.spring.project.board;
 
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BoardDAO {
 
+		@Autowired
+		SqlSessionTemplate sqlSessionTemplate; // 필드
+
+		public int insert(Map<String, Object> map) {
+			return this.sqlSessionTemplate.insert("tblBoard.insert", map);
+		}
+		
+		public Map<String, Object> selectDetail(Map<String, Object> map) {
+			 return this.sqlSessionTemplate.selectOne("tblBoard.select_detail", map);
+		}
+		
+		public List<Map<String, Object>> selectList(Map<String, Object> map) {
+			return this.sqlSessionTemplate.selectList("tblBoard.select_list", map);
+		}
+	
 }
